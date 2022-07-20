0add27aa7aa0f3bf292e9c0ec98ca4b12fb690d1
Lavanya <lavanyamuktham30@gmail.com>
2022-07-20 05:09:04 -0700

    added user to payment

--- assignment/assignment/urls.py
+++ assignment/assignment/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('facebook/', include('facebook.urls')),
+    path('payments/', include('payments.urls')),
     path('admin/', admin.site.urls),
 ]
